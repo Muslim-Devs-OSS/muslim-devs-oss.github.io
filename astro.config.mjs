7bcfff1998a483939798f13a9c3c4ee1c3c5ec74
Safwan Sadid <safwansadid.ah@gmail.com>
2026-04-26 16:34:42 +0600

    add SEO enhancements with structured data; integrate sitemap support and update robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [sitemap()],
+    trailingSlash: "never",
+    site: "https://muslim-devs-oss.github.io",
+    base: "/",
     vite: {
         plugins: [tailwindcss()]
     },
-    site: "https://muslim-devs-oss.github.io",
-    base: "/",
 });
